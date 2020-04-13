2093602f65a03de4075f2a051f1ffb11c991b0c2
raven <857697474@qq.com>
2020-04-13 22:26:46 +0800

    beifen

--- test/test.ino
+++ test/test.ino
@@ -1,9 +1,13 @@
+//ArduinoJSON库
 #include <Arduino_JSON.h>
 #include <WiFi.h>
+//MQTT 库
 #include <PubSubClient.h>
 #include <WiFiSTA.h>
+// 文件操作库
 #include "SPIFFS.h"
 #include "FS.h"
+//ESP Async WebServer库
 #include <Arduino.h>
 #ifdef ESP32
 #include <AsyncTCP.h>
@@ -12,11 +16,13 @@
 #include <ESPAsyncTCP.h>
 #endif
 #include <ESPAsyncWebServer.h>
-#include <WiFiMulti.h>
-
-WiFiMulti wifiMulti;
 AsyncWebServer server(80);
-const char* ssid = "F";
+//ArduinoHttpClient库
+#include <ArduinoHttpClient.h>
+
+//后台服务器的端口
+int port = 8080;
+//const char* ssid = "F";
 //const char* password =  "13892393046";
 //const char* password =  "11111";
 //const char* myssid = "hfuu_raven";
@@ -25,16 +31,19 @@ const char* ssid = "F";
 //const char* user = "raven";
 //const char* pass = "123456";
 const int mqttPort = 1883;
-boolean connectFlag = false;
+boolean connectFlag = true;
 
 #define arduinoSerial Serial2
 WiFiClient espClient;
-PubSubClient client(espClient);
+PubSubClient mqttClient(espClient);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 JSONVar device_jSON;
+WiFiClient wifi;
+
+
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
 }
@@ -54,13 +63,12 @@ void setup_wifi() {
     unsigned long endTime = millis();
     // 如果10s还没连上 跳出循环
     if(endTime - startTime >=10000){
-      connectFlag = true;
-      espClient.stop();
+      connectFlag = false;
       break;
     }
   }
   // 如果没有连接上WIFI
-  if(connectFlag){
+  if(!connectFlag){
     WiFi.mode(WIFI_AP);
     WiFi.softAP(device_jSON["mySsid"],device_jSON["myPassword"]);
     Serial.println("WIFI连接失败，请连接该设备热点重新配置您的信息。正在创建热点...");
@@ -70,17 +78,18 @@ void setup_wifi() {
     Serial.println((const char*)device_jSON["myPassword"]);
     Serial.println("WiFi Create Success!");
     Serial.println("IP address: ");
-    Serial.println(WiFi.softAPIP();
+    Serial.println(WiFi.softAPIP());
   }else{
+    
     randomSeed(micros());
     Serial.println("");
     Serial.println("WiFi connected Success!");
-    Serial.println("IP address: ");
+    Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-    Serial.println("云服务器地址：");
-     Serial.println(device_jSON["publicAddress"]);
-    client.setServer(device_jSON["publicAddress"], mqttPort);
-    client.setCallback(callback);
+    Serial.print("云服务器地址：");
+    Serial.println(device_jSON["publicAddress"]);
+    mqttClient.setServer(device_jSON["publicAddress"], mqttPort);
+    mqttClient.setCallback(callback);
   }
   
 }
@@ -133,19 +142,40 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
 void reconnect() {
  
   // 如果wifi连接成功
-  while (connectFlag && !client.connected()) {
+  while (connectFlag && !mqttClient.connected()) {
+    HttpClient httpClient = HttpClient(wifi, device_jSON["publicAddress"], port);
+    String contentType = "application/x-www-form-urlencoded";
+    String postData = "username="+JSON.stringify(device_jSON["username"])+"&password="+JSON.stringify(device_jSON["password"]);
+    httpClient.post("/mqtt/getMqttInfo", contentType, postData);
+    int statusCode = httpClient.responseStatusCode();
+    String response = httpClient.responseBody();
+    String mqttUsername;
+    String mqttPassword;
     Serial.print("Attempting MQTT connection...");
     // 创建一个随机id
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
+    if(statusCode == 200){
+       JSONVar req =  JSON.parse(response);
+       Serial.println(JSON.stringify(req));
+       mqttUsername = req["data"]["mqtt_username"];
+       mqttPassword = req["data"]["mqtt_password"];
+       Serial.print("请求到的mqtt账号：");
+       Serial.println(mqttUsername);
+       Serial.print("请求到的mqtt密码：");
+       Serial.println(mqttPassword);
+    }else{
+      Serial.print("请求mqtt信息异常！");
+    }
     // 开始连接mqtt服务器
-    if (client.connect(clientId.c_str(),(const char*)device_jSON["username"],(const char*)device_jSON["password"])) {
+    if (mqttClient.connect(clientId.c_str(),mqttUsername.c_str(),mqttPassword.c_str())) {
       Serial.println("MQTT connected success!");
       //订阅消息
-      client.subscribe(device_jSON["deviceId"]);
+      mqttClient.subscribe(device_jSON["deviceId"]);
+      
     } else {
       Serial.print("MQTT connected failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqttClient.state());
       Serial.println(" try again in 5 seconds");
       delay(5000);
     }
@@ -196,23 +226,7 @@ void init(fs::FS &fs){
   Serial.println(driver);
   device_jSON = JSON.parse(driver);
 }
-// 写入文件
-boolean writeFile(fs::FS &fs, const char * path, String message){
-    Serial.printf("Writing file: %s\r\n", path);
 
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return false;
-    }
-    if(file.print(message)){
-        Serial.println("- file written");
-        return true;
-    } else {
-        Serial.println("- frite failed");
-        return false;
-    }
-}
 //启动web服务器
 void startWebServer(){
     server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
@@ -246,19 +260,41 @@ void setup() {
 }
 
 void loop() {
-
-  if (!client.connected()) {
+  // 若未连接上，则一直连接WIFI
+  if (!mqttClient.connected()) {
     reconnect();
   }
-  client.loop();
+  mqttClient.loop();
 
   unsigned long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 10000) {
     lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
+    HttpClient httpClient = HttpClient(wifi, device_jSON["publicAddress"], port);
+    httpClient.get("/device/keepOnline?deviceId="+device_jSON["deviceId"]);
+    //++value;
+    //snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    //client.publish("outTopic", msg);
+    //Serial.print("Publish message: ");
+    //Serial.println(msg);
+    
+   
   }
+}
+
+// 写入文件
+boolean writeFile(fs::FS &fs, const char * path, String message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return false;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+        return true;
+    } else {
+        Serial.println("- frite failed");
+        return false;
+    }
 }
